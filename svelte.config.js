dccb018687b8c14a54a59cde73416553838f195c
frizelle-cheminfo <mitchellfrizelle@outlook.com>
2025-04-21 21:11:37 +0000

    Restore svelte.config.js with SPA fallback

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+export default {
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+    prerender: {
+      default: true
+    }
+  }
+};
